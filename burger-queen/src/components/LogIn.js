68af996d93df1fd4a2af00000d8e6980bc7f9ff2
nangebav <nangebav@gmail.com>
2021-06-12 11:48:48 -0500

    vista log in sass

--- burger-queen/src/components/LogIn.js
+++ burger-queen/src/components/LogIn.js
@@ -1,16 +1,20 @@
-import logoBurgerQueen from '../images/burger-combo.png';
+import logoBurgerQueen from '../images/burger-queen-logo.png';
 import comboBQ from '../images/burger-combo.png';
+import '../styles/components/LogIn.scss';
 
 function LogIn() {
     return (
       <div className="logIn">
-        <header className="App-header">
-          <img src={logoBurgerQueen} className="App-logo" alt="logo" />
-          <p>Correo:</p>
-          <input type="text"/>
-          <p>Contraseña:</p>
-          <input type="text"/>
-          <img src={comboBQ} alt="combo Buer"/>
+        <header className="logIn-header">
+        <img src={logoBurgerQueen} className="App-logo" alt="logo" />
+          <form className="logIn-form">
+            <p>Correo:</p>
+            <input type="text" className="inputLogIn"/>
+            <p>Contraseña:</p>
+            <input type="text" className="inputLogIn"/>
+            <button className="logIn-button"> INGRESAR </button>
+          </form>
+          <img src={comboBQ} alt="inputLogIn"/>
         </header>
       </div>
     );
