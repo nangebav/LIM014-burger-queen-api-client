956ad95c61f0ba85c29ee5eacbbfbb28023bf35b
Astrid Timana <astrid.1295@gmail.com>
2021-06-24 17:55:04 -0500

    cambios unitary Product

--- burger-queen/src/components/unitaryProduct.js
+++ burger-queen/src/components/unitaryProduct.js
@@ -5,29 +5,29 @@ import { useLocalStorage } from '../Services/localFx';
 
 function UnitaryProduct(props){
 
-    const [checkedItems, setCheckedItems] = useLocalStorage('checkedItems', [])
+    // const [checkedItems, setCheckedItems] = useLocalStorage('checkedItems', [])
     const [isChecked, setIsChecked] = useState(false);
     const [quantity, setNumber] = useState(1);
 
-    const [datos, setDatos] = useState({ })
+    // const [datos, setDatos] = useState({ })
 
-    const traerDatos = (event)=>{
-        event.preventDefault()
-        if(isChecked){
+    // const traerDatos = (event)=>{
+    //     event.preventDefault()
+    //     if(isChecked){
 
-           setCheckedItems(datos)
-            console.log(checkedItems)
-        }
-    }
+    //        setCheckedItems(datos)
+    //         console.log(checkedItems)
+    //     }
+    // }
 
-    useEffect(()=>{
-        document.textContent = `${quantity}`;
-        setDatos({
-            productName: props.name,
-            productTotalPrice: props.price*quantity,
-            productQuantity:quantity
-        })
-    }, [props, quantity])
+    // useEffect(()=>{
+    //     document.textContent = `${quantity}`;
+    //     setDatos({
+    //         productName: props.name,
+    //         productTotalPrice: props.price*quantity,
+    //         productQuantity:quantity
+    //     })
+    // }, [props, quantity])
     
     
     const handleOnChange = () => {
@@ -44,7 +44,7 @@ function UnitaryProduct(props){
                 className="checkbox-round"  
                 checked={isChecked}
                 onChange={handleOnChange}
-                onClick={traerDatos}>
+                /*onClick={traerDatos}*/>
             </input>
             <span className="prueba"> </span>
             <label htmlFor={props._id}> {props.name}</label>
