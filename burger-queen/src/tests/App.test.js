e402c0889bcab6f714ca8dc9998026e9056d35a5
Astrid Timana <astrid.1295@gmail.com>
2021-07-09 10:52:59 -0500

    avance services

--- burger-queen/src/tests/App.test.js
+++ burger-queen/src/tests/App.test.js
@@ -6,3 +6,29 @@
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+
+import axios from 'axios';
+ 
+import { getOrders } from '../Services/orders';
+ 
+jest.mock('axios');
+ 
+describe('getOrders', () => {
+    it('fetches successfully data from an API', async () => {
+        const data = {};
+     
+        axios.get.mockImplementationOnce(() => Promise.resolve(data));
+     
+        await expect(getOrders()).resolves.toEqual(data);
+    });
+ 
+      it('fetches erroneously data from an API', async () => {
+        const errorMessage = 'Network Error';
+     
+        axios.get.mockImplementationOnce(() =>
+          Promise.reject(new Error(errorMessage)),
+        );
+     
+        await expect(getOrders()).rejects.toThrow(errorMessage);
+      });
+});
